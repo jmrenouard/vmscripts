f37cd854190501498dc59049cb3918033c762671
Jean-Marie Renouard <jmrenouard@gmail.com>
2024-06-01 22:41:43 +0200

    Update installSoft_ubuntu.sh

--- installSoft_ubuntu.sh
+++ installSoft_ubuntu.sh
@@ -103,6 +103,11 @@ chmod +x minikube
 sudo mkdir -p /usr/local/bin/
 sudo install minikube /usr/local/bin/
 echo 'PATH=$PATH:/usr/local/bin' >> $HOME/.bashrc
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
 }
 
 [ "$1" = "" -o "$1" = "initsoft" ] && init_soft
